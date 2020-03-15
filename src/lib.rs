859c398fd3105cbb7c687c0a3ffca74255c3dd2c
caelunshun <caelum12321@gmail.com>
2020-03-15 11:53:45 -0600

    Reexport Legion query types

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,10 @@ mod world;
 pub use builder::{BuiltEntity, EntityBuilder};
 pub use fecs_macros::system;
 pub use legion::entity::Entity;
-pub use query::{Query, QueryBorrow, QueryElement};
+// pub use query::{Query, QueryBorrow, QueryElement};
 pub use resources::{Ref, RefMut, Resources};
 pub use system::{Executor, RawSystem};
 pub use world::World;
+
+pub use legion::filter::filter_fns::*;
+pub use legion::query::{IntoQuery, Read, TryRead, TryWrite, Write};
